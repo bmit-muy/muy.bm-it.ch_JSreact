c65c8727a33e6081c52b5eed9501cd3798c3d2b5
Müller <yannick.mueller@EDU.LAN>
2023-10-02 14:19:01 +0200

    Navigation Update

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -2,13 +2,13 @@ function Navigation() {
 return <nav className="nav">
     <ul>
         <li>
-        <a href="/" className="site-title">Home</a>
+        <a href="#Welcome" className="site-title">Home</a>
         </li>
         <li>
         <a href="/know_how">Know How</a>
         </li>
         <li>
-        <a href="/about">About Me</a>
+        <a href="#Contact">About Me</a>
         </li>
         <li>
         <a href="/protjects">Protjects</a>
